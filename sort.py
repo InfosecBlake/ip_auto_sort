37b9108813eb71ab73e9085a34c1c06d9336012f
InfosecBlake <87310427+InfosecBlake@users.noreply.github.com>
2021-09-05 08:15:08 -0500

    Update sort.py
    Remove unnecessary functions, removed sys library, added ability to specify output <filepath>.

--- sort.py
+++ sort.py
@@ -1,34 +1,22 @@
-import sys
 import argparse
-import datetime
-
-now = datetime.datetime.now()
-current_date = str(now.month) + "_" + str(now.day) + "_" + str(now.year)
 
 def main():
     parser = argparse.ArgumentParser(description='Sort IP address in a file.')
-    parser.add_argument('file', metavar='<filepath>', type=str, nargs='+',
+    parser.add_argument('file', metavar='<filepath>', type=str,
                         help='file containing IP addresses to be sorted')
-    parser.add_argument('-o', '--output', action='store_true',
-                        help='output results to a file')
+    parser.add_argument('-o', '--output', help='output results to a file of your choice')
     args = parser.parse_args()
 
-    def sort():
-        with open(sys.argv[1],"r") as f:
-            ip_list = [ip.strip() for ip in f]
+    if args.file:
+        with open(args.file,"r") as i:
+            ip_list = [ip.strip() for ip in i]
         sort_ip = sorted(ip_list, key = lambda ip: [int(ip) for ip in ip.split(".")])
         for ip in sort_ip:
             print(ip)
         if args.output:
-            with open('output' + current_date + '.txt', 'w') as i:
+            with open(args.output, 'w') as o:
                 for ip in sort_ip:
-                    i.writelines('%s\n' % ip)
-        if len(sys.argv) > 3:
-            print('Invalid input use -h for help')
-            sys.exit()
-    
-    if args.file:
-        sort()
+                    o.writelines('%s\n' % ip)
 
 if __name__=='__main__':
     main()
